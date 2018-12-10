5ce441fd133f941c92181b02f4fae04485946d71
Toqeer..Ahmad <Toqeer.Ahmad@ccjk.technology>
2018-12-10 17:59:53 +0500

    chanes

--- platforms/android/app/src/main/assets/app/app/signup/signup.component.ts
+++ platforms/android/app/src/main/assets/app/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { sharedServices } from '../shared/shared-services.service';
 
 @Component({
   selector: 'ns-signup',
@@ -8,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
   
+    nameValue :  string;
+    emailValue : string;
+    phoneValue : string;
+    passwordValue : string;
+
   constructor() {}
-  
-  ngOnInit() {
+
+  submit(){
+    alert(this.emailValue +' '+ this.passwordValue);
   }
 
+
+  ngOnInit() {
+  }
 }
